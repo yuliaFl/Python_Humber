830d6fa5d5336d21407b5425210639d366263121
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-09-18 09:47:20 -0400

    first commit

---
+++ lab_1/lab1.py
@@ -0,0 +1,34 @@
+# Create variable named x and assign value  4
+x=4
+# Create variable named y and assign value  2
+y=2
+# Print x+y
+print(x+y)
+# Create a variable named car_name and assign value Mercedes
+car_name = "Mercedes"
+# Create a variable named num_wheels and assign value of 4
+num_wheels = 4
+# Print f-string with car_name and num_wheels
+print(f"car name {car_name}, num wheels{num_wheels}")
+# Create variable named name and assign value of your name
+name = "Yulia"
+# Create variable named num_apples and assign value of 24
+num_apples = 24
+# Create variable named num_friends and assign value of 7
+num_friends = 7
+#apples_per_friend
+apples_per_friend = num_apples / num_friends
+# Print f-string with name, num_apples, num_friends, and apples_per_friend
+print(f"name {name}, apples {num_apples}, friends {num_friends}, apple per friend {apples_per_friend}")
+# Create variable named bill total and assign value of 120
+bill_total= 120
+#num_people
+num_people, cost_per_person = 50, 15
+# Print f-string with num_people, bill_total, and cost_per_person formatted to 2 decimal places
+print(f"people {num_people}, bill total {bill_total}, cost per person {cost_per_person:0.2f}")
+# Print f-string with num_people, bill_total, and cost_per_person formatted to an int
+print(f"number of people {int(num_people)}, bill total {int(bill_total)}, cost per person {int(cost_per_person)}")
+# Create variable named dessert_cost and assign value = 6.55
+dessert_cost = 6.55
+# Print f-string with num_people, dessert_cost, and total_cost_of_desserts formatted to 2 decimal places
+print(f"num people {num_people}, dessert cost {dessert_cost}, total cost {dessert_cost:0.2f} ")
