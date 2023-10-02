caaf02639762a98d9043bee8f2aeee017a806652
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-10-02 09:10:44 -0400

    added folder for class examples. added lab 3, lab 4 folders

---
+++ class_example /week3.py
@@ -0,0 +1,25 @@
+#lists 
+letters =['a','b','c']
+
+matrix=[[0,1],[2,3]]
+
+zeros = [0]*100
+print(zeros)
+
+combined = zeros + letters
+print(combined)
+
+numbers=list(range(20))
+print(numbers)
+
+chars = list("Hello World")
+print(chars)
+
+print(len(chars))
+print(chars[0]) #first one
+print(chars[-1]) #last one
+
+chars[1]='9' #sub first value to 9
+print(chars)
+
+print(chars[0:5]) #start value, to value
