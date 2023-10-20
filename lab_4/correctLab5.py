3c0de569a3cd74b405963db93e926d249636f9fc
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-10-20 18:10:41 -0400

    start correct lab 5 (folder lab4)

---
+++ lab_4/correctLab5.py
@@ -0,0 +1,12 @@
+# Create dictionary objects that contains students’ information
+# Student dictionary should contain id, name, age, and school_name
+# Student dictionary should have nested dictionaries for completed courses and ongoing courses
+# On program start ask user if they would like to:
+# View all students’ information
+# View all information on specific student, ask for id if selected
+# View all ongoing grades of specific student, ask for id if selected
+# View all completed grades of specific student, ask for id if selected
+# View average completed grades of student, ask for id if selected
+# View specific grade of specific student, ask for id and course name
+# After exit the program each operation ask user if they would like to perform another query or
+# Include error handling if user id or course name does not exist
