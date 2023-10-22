290883583b642622cffc8bfa9233c00049c2a475
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-10-21 21:50:53 -0400

    completed lab5

--- lab_4/correctLab5.py
+++ lab_4/correctLab5.py
@@ -25,6 +25,38 @@ add_student(students, student3)
 student4= create_student(444,"Kira", 18, "Humber" )
 add_student(students, student4)
 
+student1["ongoing_courses"]["Math"] = 90
+student1["ongoing_courses"]["Science"] = 85
+student1["ongoing_courses"]["History"] = 75
+
+student2["ongoing_courses"]["Math"] = 80
+student2["ongoing_courses"]["Science"] = 58
+student2["ongoing_courses"]["History"] = 57
+
+student3["ongoing_courses"]["Math"] = 70
+student3["ongoing_courses"]["Science"] = 75
+student3["ongoing_courses"]["History"] = 88
+
+student4["ongoing_courses"]["Math"] = 70
+student4["ongoing_courses"]["Science"] = 85
+student4["ongoing_courses"]["History"] = 68
+
+student1["completed_courses"]["Java"] = 90
+student1["completed_courses"]["Python"] = 85
+student1["completed_courses"]["Network"] = 75
+
+student2["completed_courses"]["Java"] = 80
+student2["completed_courses"]["Python"] = 58
+student2["completed_courses"]["Network"] = 57
+
+student3["completed_courses"]["Java"] = 70
+student3["completed_courses"]["Python"] = 75
+student3["completed_courses"]["Network"] = 88
+
+student4["completed_courses"]["Java"] = 70
+student4["completed_courses"]["Python"] = 85
+student4["completed_courses"]["Network"] = 68
+
 # View all students’ information
 def view_all_students(students):
     for student in students.values():
@@ -52,11 +84,47 @@ def view_one_student(students, student_id):
     else:
         print("Student not found")
 # View all ongoing grades of specific student, ask for id if selected
+def view_ongoing_grades(students, student_id):
+    student = students.get(student_id)
+    if student:
+        print("Ongoing Courses and Grades for Student:", student["name"])
+        for course, grade in student["ongoing_courses"].items():
+            print(f"Course: {course}, Grade: {grade}")
+    else:
+        print("Student not found")
 # View all completed grades of specific student, ask for id if selected
+def view_completed_grades(students, student_id):
+    student = students.get(student_id)
+    if student:
+        print("Completed Courses and Grades for Student:", student["name"])
+        for course, grade in student["completed_courses"].items():
+            print(f"Course: {course}, Grade: {grade}")
+    else:
+        print("Student not found")
 # View average completed grades of student, ask for id if selected
+def view_average(students, student_id):
+    student = students.get(student_id)
+    if student:
+        completed_courses = student["completed_courses"]
+        if completed_courses:
+            average_grade = sum(completed_courses.values()) / len(completed_courses)
+            print(f"Average: {average_grade:.2f}")
+        else:
+            print("No completed courses")
+    else:
+        print("Student not found")
 # View specific grade of specific student, ask for id and course name
-# After exit the program each operation ask user if they would like to perform another query or
-# Include error handling if user id or course name does not exist
+def view_specific_grade(students, student_id, course_name):
+    student = students.get(student_id)
+    if student:
+        completed_courses = student["completed_courses"]
+        grade = completed_courses.get(course_name)
+        if grade is not None:
+            print(f"Grade for Student {student['name']} in Course {course_name}: {grade}")
+        else:
+            print("Course not found")
+    else:
+        print("Student not found")
 
 # On program start ask user if they would like to:
 while True:
@@ -71,12 +139,28 @@ while True:
 
     choice=input("Choose one: ")
 
-
     if choice == "1":
         view_all_students(students)
     elif choice == "2":
-       student_id = int(input("Enter the student ID: "))
-       view_one_student(students, student_id)
+        student_id = int(input("Enter the student ID: "))
+        view_one_student(students, student_id)
+    elif choice == "3":
+        student_id = int(input("Enter the student ID: "))
+        view_ongoing_grades(students, student_id)
+    elif choice == "4":
+        student_id = int(input("Enter the student ID: "))
+        view_completed_grades(students, student_id)
+    elif choice == "5":
+        student_id = int(input("Enter the student ID: "))
+        view_average(students, student_id)
+    elif choice == "6":
+        student_id = int(input("Enter the student ID: "))
+        course_name = input("Enter the course name: ")
+        view_specific_grade(students, student_id, course_name)
     elif choice == "7":
         print("Exiting the program")
         break
+    else:
+        print("Invalid. Try again")
+# After exit the program each operation ask user if they would like to perform another query or
+# Include error handling if user id or course name does not exist
