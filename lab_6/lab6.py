de874c71c9e2c25f09c8e799dac9126ce513c66b
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-11-06 21:57:28 -0500

    touch us on lab 6

--- lab_6/lab6.py
+++ lab_6/lab6.py
@@ -31,7 +31,6 @@ try:
 except: 
     print('soccer error')
 
-'''
 # Define file paths for intersection, union, and difference files
 intersection_basketball_soccer_file = 'data/intersection_basketball_soccer.txt'
 union_basketball_baseball_file = 'data/union_basketball_baseball.txt'
@@ -39,31 +38,39 @@ difference_basketball_soccer_file = 'data/difference_basketball_soccer.txt'
 difference_baseball_basketball_file = 'data/difference_baseball_basketball.txt'
 
 # Use with statement and set intersection to write intersection_basketball_soccer.txt
-with open(intersection_basketball_soccer_file, 'w') as f:
-    intersection = basketball_team.intersection(soccer_team)
-    f.write('\n'.join(intersection))
+try: 
+    with open(intersection_basketball_soccer_file, 'w') as f:
+        intersection = basketball_team.intersection(soccer_team)
+        f.write('\n'.join(intersection))
+except:
+    print('basketball soccer intersection error')
 
 # Use with statement and set union to write union_basketball_baseball.txt
-with open(union_basketball_baseball_file, 'w') as f:
-    union = basketball_team.union(baseball_team)
-    f.write('\n'.join(union))
+try:
+    with open(union_basketball_baseball_file, 'w') as f:
+        union = basketball_team.union(baseball_team)
+        f.write('\n'.join(union))
+except:
+    print('basketball baseball union error')
 
 # Use with statement and set difference to write difference_basketball_soccer.txt
-with open(difference_basketball_soccer_file, 'w') as f:
-    difference = basketball_team.difference(soccer_team)
-    f.write('\n'.join(difference))
+try: 
+    with open(difference_basketball_soccer_file, 'w') as f:
+        difference = basketball_team.difference(soccer_team)
+        f.write('\n'.join(difference))
+except: 
+    print('basketball soccer difference union')
 
 with open(difference_baseball_basketball_file, 'w') as f:
     difference = baseball_team.difference(basketball_team)
     f.write('\n'.join(difference))
-
 # Create 3 level nested tuple e.g. (1, 2, (3, 4, (6, 7)))
 nested_tuple = (1, 2, (3, 4, (6, 7)))
 
 # Use with statement to write nested tuple to file
 nested_tuple_file = 'data/nested_tuple.txt'
 with open(nested_tuple_file, 'w') as f:
-    file.write(str(nested_tuple))
+    f.write(str(nested_tuple))
 # Use loop to create flattened tuple and write to file using with
 # Ensure files are closed and exceptions caught
 # Flatten the nested tuple
@@ -80,8 +87,8 @@ flattened_tuple = flatten_tuple(nested_tuple)
 
 # Write the flattened tuple to a file
 flattened_tuple_file = 'data/flattened_tuple.txt'
-with open(flattened_tuple_file, 'w') as file:
-    file.write(str(flattened_tuple))
+with open(flattened_tuple_file, 'w') as f:
+    f.write(str(flattened_tuple))
 # Ask user for range for numbers
 start = int(input("Enter start number: "))
 end = int(input("Enter end number: "))
@@ -90,7 +97,7 @@ end = int(input("Enter end number: "))
 even_numbers = [x for x in range(start, end + 1) if x % 2 == 0]
 even_numbers_file = 'data/even_numbers.txt'
 with open(even_numbers_file, 'w') as f:
-    file.write('\n'.join(map(str, even_numbers)))
+    f.write('\n'.join(map(str, even_numbers)))
 
 # Ask user for range for numbers
 start = int(input("Enter start number: "))
@@ -101,5 +108,4 @@ squared_numbers = [x**2 for x in range(start, end + 1)]
 
 squared_numbers_file = 'data/squared_numbers.txt'
 with open(squared_numbers_file, 'w') as f:
-    file.write('\n'.join(map(str, squared_numbers)))
-    '''
+    f.write('\n'.join(map(str, squared_numbers)))
